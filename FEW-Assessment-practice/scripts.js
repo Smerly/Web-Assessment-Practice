38378f32f46ba87af2299fa9d8b426d96ed92543
Smerly <69550699+Smerly@users.noreply.github.com>
2020-10-06 13:44:49 -0700

    Add files via upload

---
+++ FEW-Assessment-practice/scripts.js
@@ -0,0 +1,39 @@
+const inputWidth = document.querySelector("#width");
+const inputHeight = document.querySelector("#height");
+const inputColor = document.querySelector("#color");
+const displayWidth = document.querySelector("#display-width");
+const displayHeight = document.querySelector("#display-height");
+const displayColor = document.querySelector("#display-color");
+const visualHTML = document.querySelector("#visual-html");
+
+inputWidth.addEventListener("input", display, displayVisual);
+inputHeight.addEventListener("input", display, displayVisual);
+inputColor.addEventListener("input", display, displayVisual);
+
+function display(e) {
+  const width = parseFloat(inputWidth.value);
+  const height = parseFloat(inputHeight.value);
+  const color = inputColor;
+
+  displayWidth.innerHTML = width.toFixed(2);
+  displayHeight.innerHTML = height.toFixed(2);
+  displayColor.innerHTML = color.value;
+}
+
+function displayVisual(e) {
+  //   border.style.width = `${inputWidth.value}px`;
+  //   border.style.height = `${inputHeight.value}px`;
+  //   border.style.color = `${inputColor.value}px`;
+
+  const width = parseFloat(inputWidth.value);
+  const height = parseFloat(inputHeight.value);
+  const color = inputColor;
+
+  const visual = `style='width: ${width}px;
+                         height: ${height}px;
+                         color: ${color};`;
+  visualHTML.innerHTML = visual;
+}
+
+display(e);
+displayVisual(e);
